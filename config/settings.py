5d7827d888b7c451471a06631eb8066eadb5fa04
Rafael andres Rossel galarza <rafaelandresrosselgalarza@MacBook-Air-de-Rafael.local>
2025-05-09 06:35:11 -0500

    activacion de cors en config/settings

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'core',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -62,7 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+  
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
